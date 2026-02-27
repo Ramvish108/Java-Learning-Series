c290b4efc6c8008adc5f20ba7671a4206492bf77
Ramvish108 <rv7029919@gmail.com>
2026-02-27 17:19:54 +0530

    Day 05 - Beginner - Table

---
+++ Day-05-Table/Table.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Table {
+public static void main(String[] args) {
+    System.out.println("enter num whose table want you to print");
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    for (int i=1;i<=10;i++){
+        System.out.println(a+" X "+i+" = "+a*i);
+
+    }
+
+
+
+}    
+}
